6a2e08ddd86923c2f1a3cc30d62699c7ebbbcb83
Eng Ahmed Elhaddad <eng.ahmed@advancedsystems-int.com>
2026-03-06 20:55:29 +0200

    fix datasheet engine

--- main.py
+++ main.py
@@ -87,9 +87,7 @@ def get_product(part_number: str):
 
     intelligence = detect_part_info(part_number)
     datasheet = get_datasheet(part_number)
-
-     results = search_local(part_number)
-
+    results = search_local(part_number)
     if results:
         product = results[0]
 
